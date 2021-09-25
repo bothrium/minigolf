97970ae1b773fa7267e5c67123fb9cde8733ef9d
loomedai <bbrith@hotmail.com>
2021-09-25 17:28:47 +0200

    ul

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -1,37 +1,5 @@
-const score = document.addEventListener('keydown')
-
-var drawCounter = function () {
-    document.getElementById('counter').innerHTML = store.counter
-}
-
-var bindEvents = function () {
-    document.getElementById('increase').addEventListener('click', function () {
-        increase()
-        drawCounter()
-    })
-    document.getElementById('decrease').addEventListener('click', function () {
-        decrease()
-        drawCounter()
-    })
-}
-
-
-
-var store = {
-    counter: 0
-}
-
-// 데이터 조작 함수
-var increase = function () {
-    store.counter++
-}
-
-var decrease = function () {
-    store.counter--
-}
-
-// 앱 실행 함수
-var init = function () {
-    bindEvents()
-    drawCounter()
+export default class scoreBoard{
+    constructor() {
+        this.ul = document.querySelector('ul#score')
+    }
 }
