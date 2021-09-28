c5897cce83fe148c306496d7a12ec8a43df92b0d
loomedai <bbrith@hotmail.com>
2021-09-28 11:02:01 +0200

    knap

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -1,5 +1,12 @@
 export default class scoreBoard{
+
+
+
     constructor() {
         this.ul = document.querySelector('ul#score')
     }
 }
+
+const scoreGenerator = document.createElement( function{
+
+});
