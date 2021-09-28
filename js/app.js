951b608bacac620cc7869a8d1d02a8f6d3ac6cc5
bothriumeh@gmail.com <monster55>
2021-09-28 11:26:09 +0200

    test

--- js/app.js
+++ js/app.js
@@ -36,3 +36,4 @@ const init = function () {
 }
 
 init()
+
--- js/localstorage.js
+++ js/localstorage.js
@@ -1,5 +1,17 @@
 export default class Localstorage{
     constructor() {
+        this.enterButton = document.querySelector('#inputArea button');
+        this.input = document.querySelector('#inputArea input');
+        this.ul = document.querySelector('ul#toDoList');
+
+        this.enterButton.addEventListener('click', (e) => this.addListItem(e));
+        this.input.addEventListener('keypress', (e) => this.addListItem(e));
+    }
+
+    addListItem(e){
+        if(this.input.value.length > 0 && (e.key === 'Enter' || e.key === undefined)){
+            this.createListItem();
+        }
 
     }
 
